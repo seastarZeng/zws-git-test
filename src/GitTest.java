21236569ef4df11cba7ddf4412aa0bdbc356b8d5
曾维森 <zengweisen@jmfen.cn>
2019-12-30 15:58:36 +0800

    init dev

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,22 +5,7 @@
  * @Author zws
  */
 public class GitTest {
-//    dev 新增加文件
 
-    //dev-hotfix 修复提交
-
-//    merge origin/dev into sit ,push sit to origin...
-
-    //sit-hotfix测试
-
-    //dev version 1
-
-    //revert 1
-
-    //dev revert 1
-
-    //dev revert 2  //dev revert3
-
-    //dev revert3
+    //init dev
 
 }
